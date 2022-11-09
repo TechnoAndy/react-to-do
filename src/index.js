1230940ae8fb54606e79ca48b24db6c09036e54c
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-09 13:01:07 +0200

    Add code for todo list from tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import TodoContainer from './Components/TodoContainer';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
   </React.StrictMode>
 );
 
